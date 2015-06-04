98681fe179fd323af45930bd4ec131c4bb8f3034
Pritish Chakraborty <chakrabortypritish@gmail.com>
2015-06-05 00:42:25 +0530

    Cleaned js and html and introduced initial design

--- youtubeDownloader/www/js/controllers.js
+++ youtubeDownloader/www/js/controllers.js
@@ -1,8 +1,6 @@
-angular.module('starter.controllers', [])
+angular.module('youDown.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
-
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('SearchCtrl', function($scope) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,18 +9,9 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
   
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  }
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('FavoritesCtrl', function($scope) {
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
 });
